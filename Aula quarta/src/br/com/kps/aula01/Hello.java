e260a8dd3398e6ab3fde030611a11712c6152361
819144193 <819144193@VLELI0107.animaedu.intranet>
2020-03-04 22:30:19 -0300

    Atualização

---
+++ Aula quarta/src/br/com/kps/aula01/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.kps.aula01;
+
+/**
+ * @author 819144193
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello mundo!");
+		System.out.println("Hello vida!");
+	}
+
+}
